a2e5abf9e7c4bc345333ad51805c7c046c165762
Jay Rainey <jawrainey@gmail.com>
2014-07-08 12:48:20 +0100

    Add basic login validation.
    - As this is a prototype encrypting passwords is not a priority. Flask-login will be used instead at a later time.

--- app/views.py
+++ app/views.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from app import app, forms
-
+from app import app, forms, models
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     login_form = forms.LoginForm()
     if login_form.validate_on_submit():
-        # TODO: validate user against db
-        flash('The form has been submitted successfully.')
-        return redirect(url_for('index'))
+        user = models.User.query.filter_by(username = login_form.username.data).first()
+        if user:
+            # Used to display user-specific nav items
+            session['logged_in'] = True
+            return redirect(url_for('create'))
+        else:
+            flash('Invalid username or password.')
     return render_template('login.html', form=login_form)
 
 
